118ced53315ae9bc777db90c11d82479442f0661
rishavs <rishav.sharan@live.com>
2016-08-25 19:00:26 +0530

    Finished neighbours and marking hexes on map edge

--- main.lua
+++ main.lua
@@ -2,72 +2,48 @@ debug = true
 
 local inspect = require "inspect"
 
-local hex_size = 100
-local hex_grid_gap = 10
-local lvl_width_hex_count = 10
-local lvl_height_hex_count = 10
+local hex_size = 10
+local hex_grid_gap = 1
+local lvl_width_hex_count = 40
+local lvl_height_hex_count = 40
 
 local mousex, mousey 
 local hex_grid_obj = {}
 local points_list = {}
-
--- format is
-
--- Sample_hex_grid_obj = { 
-    -- id = {
-        -- center = {x = x, y = y},
-        -- color = {r, g, b},
-        -- coord = {x = x, y = y, z = z}
-    -- }
--- }
-
--- local Sample_Point = {x = 200, y = 300}
--- local Sample_Points_List = {{x = 100, y = 200}, {x = 200, y = 300}, {x = 300, y = 400}}
 
 local test_hex_points ={}
 local test_points_list = {}
 
 function love.load(arg)
     test_hex_points = create_hex(300, 300, 100)
-    xx = create_hex_grid()
+    
+    -- First create the hex grid
+    create_hex_grid()
+    
+    -- Add neighbour information to each hex in grid
+    -- now make all the edge hexes as water
 end
 
 function love.draw(dt)
 
     for id, hex in pairs(hex_grid_obj) do
-        love.graphics.setColor(hex.color)
-        love.graphics.polygon(hex.fill, hex.vertices)
+        love.graphics.setColor(50, 50, 50)
+        love.graphics.polygon("line", hex.vertices)
         
-        love.graphics.setColor(255, 255, 255)
-        love.graphics.setPointSize(2)
-        love.graphics.points(hex.center.x, hex.center.y)
-        
-        -- love.graphics.print( id, hex.center.x -20, hex.center.y -20)
-        
-        love.graphics.print( "x = " .. hex.coord.q, hex.center.x + hex_size/4, hex.center.y + hex_size/4)
-        love.graphics.print( "y = " .. hex.coord.r, hex.center.x - hex_size/2, hex.center.y + hex_size/4)
-        love.graphics.print( "z = " .. hex.coord.s, hex.center.x - hex_size/4, hex.center.y - hex_size/2)
+        -- love.graphics.setColor(255, 255, 255)
+        -- love.graphics.print( "x = " .. hex.coord.q, hex.center.x + hex_size/4, hex.center.y + hex_size/4)
+        -- love.graphics.print( "y = " .. hex.coord.r, hex.center.x - hex_size/2, hex.center.y + hex_size/4)
+        -- love.graphics.print( "z = " .. hex.coord.s, hex.center.x - hex_size/4, hex.center.y - hex_size/2)
         
     end
+
+    -- love.graphics.print( id, hex.center.x -20, hex.center.y -20)
     
-    if #points_list > 0 then
-        love.graphics.points(points_list)
-    end
-    
-    if #points_list > 2 then
-        love.graphics.line(points_list)
-    end
+
     
     love.graphics.setColor(255, 255, 255)
     love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10)    
-    
-    local cQ, cR, cS = pixel_to_hex(mousex, mousey, hex_size)
-    love.graphics.print("id: ".. cQ .. "x" .. cR .. "y" .. cS .. "z", 10, 20)    
-    
-    love.graphics.print(cQ .. "x" .. cR .. "y" .. cS .. "z", mousex -30, mousey -50)    
-    
-    love.graphics.setColor(100, 255, 100)
-    love.graphics.print(mousex .. ", " .. mousey, mousex -30, mousey -30)
+
 end
 
 function love.update(dt)
@@ -80,11 +56,18 @@ function love.mousepressed(x, y, button, istouch)
         table.insert(points_list, x)
         table.insert(points_list, y)
     
-        local cQ, cR, cS = pixel_to_hex(x, y, hex_size)
-        local cSum = cQ + cR + cS
-        print("\nMouse click at: " .. x .. ", " .. y)
-        print(cQ, cR, cS, cSum)
-
+        local h_id = get_hex_id_from_point(x, y)
+        if h_id then
+            local cQ, cR, cS = h_id.q, h_id.r, h_id.s
+            local cSum = cQ + cR + cS
+            print("\nMouse click at: " .. x .. ", " .. y)
+            print("Hex Id is : " .. cQ .. "q " .. cR .. "r " .. cS .. "s and Sum = " .. cSum)
+            
+            local h_id = cQ .. "q" .. cR .. "r" .. cS .. "s"
+            local nbr = get_hex_neighbour(h_id)
+            print("Is Hex on the Edge? : ", hex_grid_obj[h_id].isOnEdge)
+            print("its neighbours are : \n" .. inspect(nbr))
+        end
     end
 end
 
@@ -118,15 +101,20 @@ function create_hex_grid()
             if cS == -0 then cS = 0 end
     
     
-            temp_hex_id = cQ .. "x" .. cR .. "y" .. cS .. "z"
+            temp_hex_id = cQ .. "q" .. cR .. "r" .. cS .. "s"
             temp_hex_points = create_hex(hexX, hexY, hex_size - hex_grid_gap/2)
             temp_hex  = {
                 center = {x = hexX, y = hexY},
                 coord = {q = cQ, r = cR, s = cS},
-                color = {get_random_color()},
-                fill = "line",
+                hexType = "empty",
+                isOnEdge = false,
                 vertices = temp_hex_points
             }
+            
+            -- mark the edge hexagons
+            if w == 0 or h == 0 or w == (lvl_width_hex_count - 1) or h ==  (lvl_height_hex_count - 1) then
+                temp_hex.isOnEdge = true
+            end
             
             hex_grid_obj[temp_hex_id] = temp_hex
             
@@ -156,7 +144,7 @@ function get_random_color()
     return math.random(50,255), math.random(50,255),  math.random(50,255)
 end
 
-function pixel_to_hex(x, y) 
+function get_hex_id_from_point(x, y) 
     rx = x - math.sqrt(3) * hex_size/2
     ry = y - hex_size
     
@@ -166,7 +154,22 @@ function pixel_to_hex(x, y)
     
     -- return cQ_float, cR_float, cS_float
     local cQ, cR, cS = get_round_hex_coord( cQ_float, cR_float, cS_float)
-    return  cQ, cR, cS
+    
+    -- check if hex exists
+    local test_hex_id = cQ .. "q" .. cR .. "r" .. cS .. "s"
+    
+    local temp_hex_obj = {
+        id = test_hex_id,
+        q = cQ,
+        r = cR, 
+        s = cS
+        
+    }
+    if hex_grid_obj[test_hex_id] then
+        return  temp_hex_obj
+    else
+        return false
+    end
 end
 
 -- Round to the nearest hex
@@ -195,9 +198,113 @@ function get_round_hex_coord(cX_float, cY_float, cZ_float)
     return fn_cX, fn_cY, fn_cZ
 end
 
-
-
 function round(num, dp)
-  local mult = 10^(dp or 0)
-  return (math.floor(num * mult + 0.5)) / mult
+    local mult = 10^(dp or 0)
+    return (math.floor(num * mult + 0.5)) / mult
+end
+
+function get_hex_neighbour(id)
+    
+    local neighbours = {}
+    local nNE = {} 
+    local nEE = {} 
+    local nSE = {} 
+    local nSW = {} 
+    local nWW = {} 
+    local nNW = {} 
+    
+    if not hex_grid_obj[id] then 
+        return false 
+    end
+    
+    local coords = hex_grid_obj[id].coord
+    local cQ, cR, cS = coords.q, coords.r, coords.s
+    print("Id : " .. cQ .. "q" .. cR .. "r" .. cS .. "s" )
+    
+    nNE.q = cQ + 1
+    nNE.r = cR - 1
+    nNE.s = cS
+    
+    nNE.id = nNE.q .. "q" .. nNE.r .. "r" .. nNE.s .. "s"
+    
+    if hex_grid_obj[nNE.id] then
+        nNE.neighbourExists = true
+    else
+        nNE.neighbourExists = false
+    end
+    
+    neighbours["NE"] = nNE
+    
+    nEE.q = cQ + 1
+    nEE.r = cR
+    nEE.s = cS - 1
+
+    nEE.id = nEE.q .. "q" .. nEE.r .. "r" .. nEE.s .. "s"
+    
+    if hex_grid_obj[nEE.id] then
+        nEE.neighbourExists = true
+    else
+        nEE.neighbourExists = false
+    end
+    
+    neighbours["EE"] = nEE
+    
+    nSE.q = cQ
+    nSE.r = cR + 1
+    nSE.s = cS - 1
+    
+    nSE.id = nSE.q .. "q" .. nSE.r .. "r" .. nSE.s .. "s"
+    
+    if hex_grid_obj[nSE.id] then
+        nSE.neighbourExists = true
+    else
+        nSE.neighbourExists = false
+    end
+    
+    neighbours["SE"] = nSE
+    
+    nSW.q = cQ -1
+    nSW.r = cR + 1
+    nSW.s =  cS
+    
+    nSW.id = nSW.q .. "q" .. nSW.r .. "r" .. nSW.s .. "s"
+    
+    if hex_grid_obj[nSW.id] then
+        nSW.neighbourExists = true
+    else
+        nSW.neighbourExists = false
+    end
+    
+    neighbours["SW"] = nSW
+    
+    nWW.q = cQ -1
+    nWW.r = cR
+    nWW.s = cS + 1
+        
+    nWW.id = nWW.q .. "q" .. nWW.r .. "r" .. nWW.s .. "s"
+    
+    if hex_grid_obj[nWW.id] then
+        nWW.neighbourExists = true
+    else
+        nWW.neighbourExists = false
+    end
+
+    neighbours["WW"] = nWW
+    
+    nNW.q = cQ
+    nNW.r = cR -1
+    nNW.s = cS + 1
+
+    nNW.id = nNW.q .. "q" .. nNW.r .. "r" .. nNW.s .. "s"
+    
+    if hex_grid_obj[nNW.id] then
+        nNW.neighbourExists = true
+    else
+        nNW.neighbourExists = false
+    end
+
+    neighbours["NW"] = nNW
+
+    return neighbours
+    
 end
